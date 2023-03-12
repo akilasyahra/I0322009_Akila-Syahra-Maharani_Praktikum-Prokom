1fb4b9bc702d07535e1f233a888a9cc5c6e3b281
SEMESTER 2 <akilasyahra@gmail.com>
2023-03-12 20:52:32 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,23 @@
+#Exercise-01
+
+print("="*55) 
+print ("\n"+"Nama   : Akila Syahra Maharani")
+print ("\n"+"Nim    : I0322009")
+print("="*55) 
+print ("""
+    Halo, perkenalkan namaku Akila Syahra Maharani, 
+biasa dipanggil Akila. Aku berasal dari Daerah Istimewa
+Yogyakarta atau yang biasa disebut Kota Jogja. Kota Jogja 
+merupakan kota kelahiranku sekaligus tempat dimana aku 
+tumbuh. Kota jogja adalah salah satu kota yang masih 
+memegang erat tradisi jawa yang kuat. Selain memiliki 
+berbagai budaya yang beragam, kuliner yang ada di kota 
+jogja juga cukup khas dan populer sehingga menarik wisatawan
+untuk berkunjung. Sebagai wilayah dengan identitas budaya
+yang kental, menjadikan kota jogja memiliki predikat sebagai
+kota budaya. 
+    Harapanku sebagai seorang mahasiswa teknik industri setelah
+lulus adalah untuk dapat memberikan hal-hal positif di lingkungan 
+serta dapat memanfaatkan ilmu yang telah aku pelajari 
+selama berkuliah di teknik industri UNS.""")
+print("="*55) 
--- latihan1.py
+++
@@ -1,1 +0,0 @@
-print("Akila yasudah")
