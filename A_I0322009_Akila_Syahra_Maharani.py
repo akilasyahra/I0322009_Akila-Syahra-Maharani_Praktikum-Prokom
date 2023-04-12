1180f595d8e9c195a23a223c4088d5b14798ec76
SEMESTER 2 <akilasyahra@gmail.com>
2023-04-12 15:50:08 +0700

    add

---
+++ A_I0322009_Akila_Syahra_Maharani.py
@@ -0,0 +1,123 @@
+
+
+print ("========================================")
+print ("       ""REPORT NILAI MAHASISWA""       ")
+
+print ("REPORT NILAI ALI")
+Ali = {
+    "fisika" : 80,
+    "biologi" : 80,
+    "matematika" : 85,
+    }
+IPK = (80 + 80 + 85)/3
+print ("rata-rata nilai Ali", IPK)
+
+if IPK >= 80:
+    print ("nilai IPK Ali adalah A")
+elif IPK >=70:
+    print ("nilai IPK Ali adalah B")
+elif IPK >=60:
+    print ("nilai IPK Ali adalah C")
+elif IPK >=50:
+    print ("nilai IPK Ali adalah D")
+else:
+    print ("nilai IPK Ali adalah E")
+
+
+
+print("""
+      """)
+
+print ("REPORT NILAI BUDI")
+Budi = {
+    "fisika" : 70,
+    "biologi" : 60,
+    "matematika" : 70,
+    }
+IPK = (70 + 60 + 70)/3
+print ("rata-rata nilai Budi", IPK)
+
+if IPK >= 80:
+    print ("nilai IPK Budi adalah A")
+elif IPK >=70:
+    print ("nilai IPK Budi adalah B")
+elif IPK >=60:
+    print ("nilai IPK Budi adalah C")
+elif IPK >=50:
+    pprint ("nilai IPK Budi adalah D")
+else:
+    print ("nilai IPK Budi adalah E")
+
+
+print("""
+      """)
+
+print ("REPORT NILAI CACA")
+Caca = {
+    "fisika" : 75,
+    "biologi" : 80,
+    "matematika" : 75,
+    }
+IPK = (75 + 80 + 75)/3
+print ("rata-rata nilai Caca", IPK)
+
+if IPK >= 80:
+    print ("nilai IPK Caca adalah A")
+elif IPK >=70:
+    print ("nilai IPK Caca adalah B")
+elif IPK >=60:
+    print ("nilai IPK Caca adalah C")
+elif IPK >=50:
+    print ("nilai IPK Caca adalah D")
+else:
+    print ("nilai IPK Caca adalah E")
+
+    
+print("""
+      """)
+
+print ("REPORT NILAI DODO")
+Dodo = {
+    "fisika" : 85,
+    "biologi" : 70,
+    "matematika" : 80,
+    }
+IPK = (85 + 70 + 80)/3
+print ("rata-rata nilai Dodo", IPK)
+
+if IPK >= 80:
+    print ("nilai IPK Dodo adalah A")
+elif IPK >=70:
+    print ("nilai IPK Dodo adalah B")
+elif IPK >=60:
+    print ("nilai IPK Dodo adalah C")
+elif IPK >=50:
+    pprint ("nilai IPK Dodo adalah D")
+else:
+    print ("nilai IPK Dodo adalah E")
+
+print("""
+      """)
+
+print ("REPORT NILAI ELO")
+Elo = {
+    "fisika" : 85,
+    "biologi" : 70,
+    "matematika" : 80,
+    }
+IPK = (90 + 60 + 80)/3
+print ("rata-rata nilai Elo", IPK)
+
+if IPK >= 80:
+    print ("nilai IPK Elo adalah A")
+elif IPK >=70:
+    print ("nilai IPK Elo adalah B")
+elif IPK >=60:
+    print ("nilai IPK Elo adalah C")
+elif IPK >=50:
+    pprint ("nilai IPK Elo adalah D")
+else:
+    print ("nilai IPK Elo adalah E")
+
+
+print ("========================================")
